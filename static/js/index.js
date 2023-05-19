0c331a58a3c8d0ec45af6a2270a585a117b55b45
Jayden Viegas <jaydenviegas57@gmail.com>
2023-05-19 12:54:58 +0530

    Initial commit

---
+++ static/js/index.js
@@ -0,0 +1,9 @@
+function displayForm(){
+    document.getElementById('form').style.display="block"
+    document.getElementById('table').style.display="none"
+}
+
+function displayTable(){
+    document.getElementById('form').style.display="none"
+    document.getElementById('table').style.display="block"
+}
